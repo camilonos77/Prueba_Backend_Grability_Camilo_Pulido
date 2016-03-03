b069ce18dc1f1291e46673233fc54f3d12bd8fa7
camilonosdocs@gmail.com <camilonosdocs@gmail.com>
2016-03-03 16:48:53 -0500

    Funcionamiento de iniciar cubo

--- CODING_CHALLENGE/static/js/app.js
+++ CODING_CHALLENGE/static/js/app.js
@@ -35,10 +35,37 @@ module.config(['$routeProvider',
  * */
 module.run(function($rootScope,$location) {
 
-	console.log("dasd");
+	console.log("Iniciando App");
 	
 });
 
+
+
+
+/*
+ * desarrollador por: Camilo Andres Pulido
+ * email: camilonosdocs@gmail.com
+ * Valida el ingreso de numeros enteros
+ * */
+module.directive('restrict', function($parse, $timeout) {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, iElement, iAttrs, controller,ngModel) {
+        	
+            scope.$watch(iAttrs.ngModel, function(value) {
+                if (!value) {
+                    return;
+                }
+             
+	            value = value.toString();
+	            var a =  value.replace(/[A-Za-z$.;:ÃƒÂ±@Ãƒâ€˜^~\<\Ã±\Ã‘\>`\ñ\Ñ\,_\Ã¢â€šÂ¬\(\Ã‚Âº\Ã‚Âª\$\%\&\)\=\Ã¢Ë†Å¾\-\Ã‚Â·\_\?\Ã‚Â¿\Ã‚Â¨\[\]\+\*\ÃƒÂ±\Ãƒâ€˜\}\{\Ã‚Â´\#\"\?\Ã‚Â¡\!\\\/\Ã‚Â°\Ã‚Â¬\|]/g,"").replace(/\s/g, "") 
+				scope[iAttrs.ngModel] = a;
+				iElement.val(a);
+           });
+        }
+    };
+});
 
 /*
  * Controlador del App
@@ -46,9 +73,48 @@ module.run(function($rootScope,$location) {
  */
 module.controller("indexController", function($scope,$filter) {
 	
+	$scope.cubo = [];
+	$scope.mostrarForm = true;
 	
-	console.log("controlador");
+	$scope.iniciar = function(){
+		
+		if($scope.dimension === "1"){
+			alert("La dimensión debe ser mayor a uno ");
+			$scope.dimension = "";
+			$scope.pruebas = "";
+		}else{
+			
+			if($scope.pruebas === "1"){
+				alert("La dimensión debe ser mayor a uno ");
+				$scope.dimension = "";
+				$scope.pruebas = "";
+			}else{
+				
+				
+				$scope.mostrarForm  = false;
+				// Se inicia el 
+				$scope.iniciarCubo();
+				
+			}
+			
+			
+			
+		}
+		
+		
+	};
 	
+	
+	$scope.iniciarCubo = function(){
+		
+		for(var a = 1;a <= parseInt($scope.dimension) ; a ++){
+			
+			console.log("Número de iteraciones ");
+			$scope.cubo[""+a+""+a+""+a+""+a] = 0;
+		}
+		console.log($scope.cubo)
+		
+	}
 	
 });	
 
